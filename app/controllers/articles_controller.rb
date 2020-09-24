d6d8bf444c72ac2e40863122ba963bef1a183e5e
matbotW <mathewmcdade@gmail.com>
2020-09-23 23:16:05 -0700

    Comment model started

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,20 +9,40 @@ class ArticlesController < ApplicationController
   end
   # new
   def new
+    @article = Article.new
   end
   # edit
+  def edit
+    @article = Article.find(params[:id])
+  end
   # create
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   # update
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   # destroy
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
   #
-
-
 
   private
   def article_params
